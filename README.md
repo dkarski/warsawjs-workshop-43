c02d978bed7778bac6920edc13659c41dc9e89bb
Daniel Karski <daniel.karski5q@gmail.com>
2020-02-22 12:31:39 +0100

    Create init app.js script

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# warsawjs-workshop-41
-WarsawJs Workshop#41 - JavaScript basics
+# warsawjs-workshop-43
+WarsawJS Workshop#43
+
+## slides - live
+  - https://slides.com/danielkarski/walidacja-warsztatu/live
+
+## Aplikacja
+
+### Zadania
+1. Wydziel z głównego skryptu aplikacji app.js mniejsze moduły np wykorzystując module pattern bazujący na IIFE albo ECMAScript Module
+1. Dodaj funkcje zwrotne głównym pliku app.js. Obsłuż:
+    - dodanie nowej notatki
+    - zmiane tytułu notatki (event: change)
+    - zmiane głównego kontentu notatki (event: change)
+    - zmiane stanu aktywnej notatki z listy notatek
+1. Zaciągnij liste notatek z serwera wykorzystująć fetchAPI
+    - wyświetl główne okno alpikacji dopiero po zaciągnieciu notatek
+    - dokumentacja API https://documenter.getpostman.com/view/2154709/Szf6WTUS?version=latest
+    - zwróć uwagę, by podać parametr userId w adresie https://api.luck.org.pl/api/v2/notes?userId=dkarski
+    - API zwraca zawsze pustą liste z inicjalną notatką , gdy nie zna `userId`
+1. Obsłuż dodanie nowej notatki za pomocą API
+    - zwróć uwagę, by dodać userId do body notatki
+1. Obsłuż zmiane tytułu i notatki za pomocą API
+
+### Dodatkowo
+1. Wydziel metody do komunikacji z serwerem do osobnego modułu
+1. Wydziel główny szablon aplikacji do osobnego modułu
+1. Dodaj setInterval do za symulowania automatycznego zapisu notatki
+    - możesz przed wysłaniem żądania sprawdzić, czy notatka się zmieniła z stanem poprzednim
+    
+### Postman API documentation
+  - https://documenter.getpostman.com/view/2154709/Szf6WTUS?version=latest
---
+++ app.js
@@ -0,0 +1,77 @@
+(function () {
+  const element = null || document.getElementById("app");
+  const notes = [{
+    title: "",
+    content: "",
+    updatedAt: Date.now()
+  }]
+
+  let state = {notes, activeNoteIndex: 0};
+
+  window.addEventListener('DOMContentLoaded', render());
+
+  function render() {
+    element.innerHTML = `
+        <div class="container">
+          <div class="note-list-container"></div>
+          <div class="note-editor-container"></div>
+        </div>
+    `;
+    renderNoteList(document.querySelector('.note-list-container'))
+    renderNoteEditor(document.querySelector('.note-editor-container'))
+  }
+
+  function renderNoteEditor(element) {
+    element.innerHTML = `
+      <div class="note-editor">
+        <div class="note-editor__heading">
+            <p class="note-editor__heading__updateAt">${'Ostatnia zmiana: ' + new Date(state.notes[state.activeNoteIndex].updatedAt).toLocaleString("pl-PL")}</p>
+        </div>
+        <div class="note-editor__fields-container">
+          <textarea placeholder="Tytuł" class="note-editor__fields-container__title" rows="1">${state.notes[state.activeNoteIndex].title}</textarea>
+          <textarea placeholder="Zacznij pisać" class="note-editor__fields-container__content" rows="1">${state.notes[state.activeNoteIndex].content}</textarea>
+        </div>
+      </div>
+    `
+  }
+
+  function renderNotes(element) {
+    element.innerHTML = `
+        <ul class="note-list__list">
+            ${state.notes.map((note, index) => `   
+              <li class="note-list__list__item ${state.activeNoteIndex === index && 'note-list__list__item--active'}">
+                <h3 class="note-list__list__item__heading">${note.title ? note.title : "Brak tytułu"}</h3>
+                <p class="note-list__list__item__content">${note.content ? note.content : '&nbsp;'}</p>
+              </li>  
+            `).join('')}
+        </ul>
+      `
+  }
+
+  function renderNoteList(element) {
+    element.innerHTML = `
+      <div class="note-list">
+        <div class="note-list__heading">
+            <div class="note-list__heading__content-container">
+              <h2 class="note-list__heading__title">Wszystkie notatki</h2>
+              <p class="note-list__heading__count">${state.notes.length} notatki</p>
+            </div>
+            <button class="note-list__heading__button-add">
+              <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+                  <g fill="none" fill-rule="evenodd">
+                      <path d="M0 0h30v30H0z"></path>
+                      <circle cx="15" cy="15" r="14" fill="#00A82D"></circle>
+                      <rect width="14" height="2" x="8" y="14" fill="#FFF" rx="1"></rect>
+                      <rect width="2" height="14" x="14" y="8" fill="#FFF" rx="1"></rect>
+                  </g>
+              </svg>
+            
+            </button>
+        </div>
+        <div class="note-list__list-container"></div>
+      </div>
+    `
+    renderNotes(document.querySelector('.note-list__list-container'), {state})
+  }
+})();
+
